fcf87bc25d8ae22d5c553c9cb3fd62baac059fa2
Roman Herasymenko <roman.herasymenko@gmail.com>
2020-06-12 10:13:52 -0700

    Added borders.

--- SnakyWorld/SnakyWorld.cpp
+++ SnakyWorld/SnakyWorld.cpp
@@ -21,6 +21,10 @@ void loop()
    int sizeX, sizeY;
    std::tie(sizeX, sizeY) = console->getScreenSize();
 
-   Ceil ceil(console, Coordinate(sizeX - 1, sizeY - 1));
-   Line line(console, Coordinate(0, sizeY - 1), Coordinate(sizeX - 1, sizeY - 1));
+   Ceil ceil(console, Coordinate(sizeX - 3, sizeY - 3));
+   Line topLine(console, Coordinate(0, 0), Coordinate(sizeX - 1, 0));
+   Line bottomLine(console, Coordinate(0, sizeY - 1), Coordinate(sizeX, sizeY - 1));
+
+   Line backLine(console, Coordinate(sizeX - 1, 0), Coordinate(sizeX - 1, sizeY - 1));
+   Line frontLine(console, Coordinate(0, 1), Coordinate(0, sizeY - 1));
 }
